f645bd1915f647579d7b0aef21d7e1423caa08f8
Christina Yin <yinchristina373@gmail.com>
2023-11-21 14:30:42 -0800

    mount logs

--- storage/app.py
+++ storage/app.py
@@ -89,7 +89,6 @@ def process_messages():
         try:
             client = KafkaClient(hosts=hostname)
             topic = client.topics[str.encode(app_config["events"]["topic"])]
-            print("hello")
             logger.info(f"successfully connected to Kafka.")
             break
         except Exception as e:
